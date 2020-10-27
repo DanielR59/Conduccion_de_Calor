5faf304193d84524a221ac768fb5f0000770b30d
DanielR59 <daniel_pumas_59@hotmail.com>
2020-10-27 11:26:53 -0600

    correccion dominio x

--- graficas.py
+++ graficas.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
-
+import numpy as np
 def plot_dominio(x_inicial,x_final,Nodos):
     """
     docstring
     """
     
     h=(x_final-x_inicial)/(Nodos+1)
-    x=[h*i for i in range(Nodos+2)]
+    #x=[h*i for i in range(Nodos+2)]
+    x=[i for i in np.arange(x_inicial,x_final+h,h)]
     plt.plot([x_inicial,x_final],[0,0], lw=10)
     plt.plot([x_inicial,x_inicial],[0,0], 'sr', )
     plt.plot([x_final,x_final],[0,0], 'sg', )
@@ -20,7 +21,8 @@ def plot_dominio2(x_inicial,x_final,N,u,Ta,Tb):
     """
     
     h=(x_final-x_inicial)/(N+1)
-    x=[h*i for i in range(N+2)]
+    x=[i for i in np.arange(x_inicial,x_final+h,h)]
+    #x=[h*i for i in range(N+2)]
     
     
     plt.scatter(x,u,c=u,cmap='plasma')
