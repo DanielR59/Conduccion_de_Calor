2f62ca3a98bc776103442ff8ac66fb78f3ea1639
DanielR59 <daniel_pumas_59@hotmail.com>
2020-10-28 00:11:16 -0600

    Mas documentacion y solucion de bug

--- graficas.py
+++ graficas.py
@@ -1,8 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 def plot_dominio(x_inicial,x_final,Nodos):
-    """
-    docstring
+    """funcion que genera el plot del doominio de longitud L definido a partir de los valores de entrada, además de graficar el numero de nodos del sistema 
+
+    Args:
+        x_inicial ([type]): Punto inicial del sistema 1D
+        x_final ([type]): Punto final del sistema 1D
+        Nodos ([type]): Numero de nodos del sistema sin contar x_inicial y x_final
     """
     
     h=(x_final-x_inicial)/(Nodos+1)
@@ -16,12 +20,24 @@ def plot_dominio(x_inicial,x_final,Nodos):
     plt.show()
 
 def plot_dominio2(x_inicial,x_final,N,u,Ta,Tb):
-    """
-    docstring
+    """Funcion que genera la grafica del resultado final de la conducción de calor en 1D estacionaria, a partir de valores de entrada
+
+    Args:
+        x_inicial ([type]): Punto inicial del sistema 1D
+        x_final ([type]): Punto final del sistema 1D
+        N ([type]): Numero de nodos del sistema en los cuales se calcula la teperatura
+        u ([type]): Solución del sistema
+        Ta ([type]): Condicion de frontera izquierda
+        Tb ([type]): Condicion de frontera derecha
     """
     
     h=(x_final-x_inicial)/(N+1)
     x=[i for i in np.arange(x_inicial,x_final+h,h)]
+    if len(u)<len(x):
+        
+        x[-2]=Tb
+        x.pop(-1)
+
     #x=[h*i for i in range(N+2)]
     
     
